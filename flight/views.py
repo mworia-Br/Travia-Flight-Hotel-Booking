6775465993e11211c70ff0b44db6ee3a3c254d04
Brian Nganga <briannganga70@gmail.com>
2023-03-02 03:00:45 +0300

    flightapibackend

--- flight/views.py
+++ flight/views.py
@@ -22,3 +22,47 @@ def select_destination(req, param):
             print(error)
     else:
         return JsonResponse({"error": "Invalid request method"})
+
+def search_offers(req):
+    if req.method == "GET":
+        try:
+            response = amadeus.shopping.flight_offers.get(
+                originLocationCode=req.GET.get("origin"),
+                destinationLocationCode=req.GET.get("destination"),
+                departureDate=req.GET.get("departureDate"),
+                adults=req.GET.get("adults"),
+                nonStop=req.GET.get("nonStop"),
+                max=req.GET.get("max")
+            )
+            context = {
+                "data": response.data
+            }
+            return JsonResponse(context)
+        except ResponseError as error:
+            print(error)
+
+def price_offer(req):
+    if req.method == "POST":
+        try:
+            flight = req.POST["flight"]
+            response = amadeus.shopping.flight_offers.pricing.post(
+                flight)
+            print(response.data)
+            return JsonResponse(response.data)
+        except ResponseError as error:
+            print(error)
+    else:
+        return JsonResponse({"error": "Invalid request method"})
+
+def book_flight(req):
+    if req.method == "POST":
+        try:
+            flight = req.POST["flight"]
+            traveler = req.POST["traveler"]
+            booking = amadeus.booking.flight_orders.post(
+                flight, traveler)
+            return JsonResponse(booking)
+        except ResponseError as error:
+            print(error)
+    else:
+        return JsonResponse({"error": "Invalid request method"})
