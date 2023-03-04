44cd232196be5eda3e18ed93826755e9534b1532
Brian Nganga <briannganga70@gmail.com>
2023-03-04 10:04:33 +0300

    test form

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,18 +22,6 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('email', 'username', 'password1', 'password2')
 
-    def save(self, commit=True):
-        user = super(SignupForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.username = self.cleaned_data['username']
-        user.password = self.cleaned_data['password1']
-
-        if commit:
-            user.save()
-        return user
-
-    
-
 class CustomPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(max_length=254, required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
 
