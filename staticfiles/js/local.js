0ee590c26dbdaedf9022894eb187e181ec144f48
Brian Nganga <briannganga70@gmail.com>
2023-03-08 01:09:59 +0300

    userin

---
+++ staticfiles/js/local.js
@@ -0,0 +1,174 @@
+let originCode = "";
+let destinationCode = "";
+let departureDate = "";
+let fromLocationArray = [];
+let toLocationArray = [];
+let flights = [];
+
+const fromLocationData = document.getElementById("fromLocationData");
+const toLocationData = document.getElementById("toLocationData");
+const flightData = document.getElementById("flightData");
+
+function handleFromLocation() {
+  let locationEl = "";
+  const fromInput = document.getElementById("from").value;
+  if (fromInput.length > 1) {
+    fetch(`https://traviabooking.azurewebsites.net/api/v1/flight/select_destination/${fromInput}`)
+      .then((response) => response.json())
+      .then((data) => (fromLocationArray = data.data));
+
+    if (fromLocationArray) {
+    fromLocationData.classList.add("location__body");
+    fromLocationData.style.display = "block";
+    const locationList = document.createElement("div");
+    locationList.classList.add("location__list");
+    fromLocationArray.forEach((location) => {
+        const locationItem = document.createElement("a");
+        locationItem.classList.add("location__item");
+        locationItem.classList.add("js-location-item");
+        locationItem.innerText = `${location.name}, ${location.address.cityName}, ${location.address.countryName}`;
+        locationItem.onclick = () => getFromLocation(location.iataCode);
+        locationList.appendChild(locationItem);
+    });
+    fromLocationData.appendChild(locationList);
+    }
+
+  fromLocationData.innerHTML = locationEl;
+}
+
+function getFromLocation(regionCode) {
+  originCode = regionCode;
+  console.log(originCode);
+  fromLocationData.style.display = "none";
+}
+
+function handleToLocation() {
+  let locationEl = "";
+  const toInput = document.getElementById("to").value;
+  if (toInput.length > 1) {
+    fetch(`https://traviabooking.azurewebsites.net/api/v1/flight/select_destination/${toInput}`)
+      .then((response) => response.json())
+      .then((data) => (toLocationArray = data.data));
+
+    if (toLocationArray) {
+        toLocationData.classList.add("location__body");
+        toLocationData.style.display = "block";
+        const locationList = document.createElement("div");
+        locationList.classList.add("location__list");
+        toLocationArray.forEach((location) => {
+            const locationItem = document.createElement("a");
+            locationItem.classList.add("location__item");
+            locationItem.classList.add("js-location-item");
+            locationItem.innerText = `${location.name}, ${location.address.cityName}, ${location.address.countryName}`;
+            locationItem.onclick = () => getToLocation(location.iataCode);
+            locationList.appendChild(locationItem);
+        });
+        toLocationData.appendChild(locationList);
+        }
+      }
+
+  toLocationData.innerHTML = locationEl;
+}
+
+function getToLocation(regionCode) {
+  destinationCode = regionCode;
+  toLocationData.style.display = "none";
+}
+
+function handleFindFlight() {
+  departureDate = document.getElementById("date").value;
+  let flightEl = "";
+
+  fetch(
+    `https://traviabooking.azurewebsites.net/api/v1/flight/search_offers/?originCode=${originCode}&destinationCode=${destinationCode}&departureDate=${departureDate}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      flights = data.data;
+
+      if (flights) {
+        flights.map((flight) => {
+         console.log(flight)
+          flightEl +=
+            '\
+         <div class="card mb-3 mt-3" >\
+         <div class="card-header">\
+           <b>Price:</b>  ' +
+            flight.price.total +
+            "  (\
+           " +
+            flight.price.currency +
+            ' )\
+         </div>\
+         <div class="card-body">\
+           Number of Seats Available:  ' +
+            flight.numberOfBookableSeats +
+            "\
+           <br />\
+           Last Ticketing Date:  " +
+            flight.lastTicketingDate +
+            "\
+           <hr />\
+           <h5>Itineraries</h5>\
+           Duration:  " +
+            flight.itineraries[0].duration +
+            ' \
+           <hr />\
+           <h5>Enter your details:</h5>\
+           <input type="text" id="first" placeholder="Your first Name" class="form-control"/>\
+           <br />\
+           <input type="text" id="last" placeholder="Your Last Name" class="form-control"/>\
+         </div>\
+         <div class="card-footer">\
+           <button class="btn btn-primary" onclick="BookFlight(flight)">Book Flight</button>\
+         </div>\
+       </div>'
+        });
+        flightData.innerHTML = flightEl;
+      } else {
+        alert("No flight Data found");
+      }
+    });
+}
+
+function BookFlight(flight) {
+  const first = document.getElementById("first").value;
+  const last = document.getElementById("last").value;
+
+  fetch("https://traviabooking.azurewebsites.net/api/v1/flight/price_offers", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      flight,
+    }),
+  })
+    .then((response) => response.json())
+    .then((dataObject) => {
+      console.log("Success:", dataObject);
+
+      fetch("https://traviabooking.azurewebsites.net/api/v1/flight/book_flight/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          flight,
+          traveler: { first, last },
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+          flights = [];
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert(error);
+    });
+}
