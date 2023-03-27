6bed78ab1c4ed98409c0feb56296ed891fce55aa
Brian Nganga <briannganga70@gmail.com>
2023-03-27 13:04:30 +0300

    run

--- flightsv2/models.py
+++ flightsv2/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class FlightTmp(models.Model):
     user_id = models.CharField(max_length=100)
-    flight_data = models.JSONField()
+    flight_data = models.JSONField(default=dict)
     added = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
